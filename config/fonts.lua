b7b8d9ae2f6cf9136278282696e0c8db86bc9666
Whit Whittle <whitwhittle@pm.me>
2025-07-27 07:44:18 -0500

    updated for my personal settings

--- config/fonts.lua
+++ config/fonts.lua
@@ -8,9 +8,9 @@ local font_size = platform.is_mac and 12 or 9
 return {
    font = wezterm.font({
       family = font_family,
-      weight = 'Medium',
+      weight = 'Regular',
    }),
-   font_size = font_size,
+   font_size = 12,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
    freetype_load_target = 'Normal', ---@type 'Normal'|'Light'|'Mono'|'HorizontalLcd'
